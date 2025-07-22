6898ca7b7f5276c690a247ee5532733bab4047da
thanos97s <48681722+thanos97s@users.noreply.github.com>
2025-07-22 09:16:05 +0300

    Update sat_prop.js

--- sat_prop.js
+++ sat_prop.js
@@ -1,27 +1,28 @@
 const sat_prop(tleSets) = async() => {
 
-const satellite = require('satellite.js');
+import * as satellite from 'satellite.js';
+//const tleData = await fetchTLE();
+//const iss = tleData.find(sat => sat.name.includes('ISS'));
+
 
   // Sample TLE
 var tleLine1 = '1 25544U 98067A   13149.87225694  .00009369  00000-0  16828-3 0  9031',
     tleLine2 = '2 25544 051.6485 199.1576 0010128 012.7275 352.5669 15.50581403831869';
 
 // Initialize a satellite record
-var satrec = satellite.twoline2satrec(tleLine1, tleLine2);
+var satrec = satellite.twoline2satrec(tleLine1, tleLine2); 
+const now = new Date();
 
-//  Propagate satellite using time since epoch (in minutes).
-var positionAndVelocity = satellite.sgp4(satrec, timeSinceTleEpochMinutes);
+//  Propagate satellite using a JavaScript Date.
+var positionAndVelocity = satellite.propagate(satrec, now);
 
-//  Or you can use a JavaScript Date
-var positionAndVelocity = satellite.propagate(satrec, new Date());
+const gmst = satellite.gstime(now);
+const positionGd = satellite.eciToGeodetic(positionAndVelocity.position, gmst);
 
-// The position_velocity result is a key-value pair of ECI coordinates.
-// These are the base results from which all other coordinates are derived.
-var positionEci = positionAndVelocity.position,
-    velocityEci = positionAndVelocity.velocity;
+const longitude = satellite.degreesLong(positionGd.longitude);
+const latitude = satellite.degreesLat(positionGd.latitude);
+const altitude = positionGd.height;
 
-
-satHeadings = tleSets.map(sat => )
-  
+console.log({ latitude, longitude, altitude });
 
 };
